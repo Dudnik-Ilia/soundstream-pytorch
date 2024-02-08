bacc37eb8cb07a9722ef7c6f2c0ecb67aaa69ffb
Dudnik-Ilia <dudnik.ilja@rambler.ru>
2024-02-08 13:58:05 +0100

    Padding problem for inference solved

--- soundstream_model.py
+++ soundstream_model.py
@@ -1,6 +1,25 @@
 from soundstream_net import Encoder, Decoder, ResidualVectorQuantizer
 import torch
 import torch.nn as nn
+import librosa
+
+
+def easy_solution_pad(x: torch.Tensor):
+    """
+    length of decoded audio is always in the form 270+n*320, where 320 is the compression factor.
+    that is why it is needed to pad the audio before correctly
+    """
+    if type(x) != torch.Tensor:
+        x = torch.Tensor(x)
+    length = x.shape[-1]
+    if length < 590:
+        raise Exception
+    pad_length = (length - 270) % 320
+    if pad_length != 0:
+        pad_length = 320 - pad_length
+    #                              Pad first dimension
+    x = torch.nn.functional.pad(x, (0, pad_length), "constant", 0)
+    return x
 
 
 class EncoderDecoder(nn.Module):
@@ -9,7 +28,7 @@ class EncoderDecoder(nn.Module):
             n_channels: int = 32,
             num_quantizers: int = 8,
             num_embeddings: int = 1024,
-            padding: str = "valid"
+            padding: str = "same"
     ):
         super().__init__()
         self.encoder = Encoder(n_channels, padding)
@@ -17,8 +36,13 @@ class EncoderDecoder(nn.Module):
         self.quantizer = ResidualVectorQuantizer(
             num_quantizers, num_embeddings, n_channels * 16)
 
-    def forward(self, x):
-        return self.encode(x)
+    def forward(self, x: torch.Tensor):
+        x = easy_solution_pad(x)
+        x = x.reshape(1, -1)
+        y = self.encode(x)
+        z = self.decode(y)
+        assert x.shape[-1] == z.shape[-1]
+        return z
 
     def encode(self, input: torch.Tensor) -> torch.Tensor:
         assert input.ndim == 2
@@ -37,13 +61,14 @@ class EncoderDecoder(nn.Module):
         return x
 
 
+
 def soundstream_16khz(checkpoint=None, pretrained=True, **kwargs):
     """SoundStream encoder decoder
     for loading from a checkpoint
     pretrained (bool): kwargs, load pretrained weights into the model
     """
     # Call the model, load pretrained weights
-    model = EncoderDecoder()
+    model = EncoderDecoder(**kwargs)
     if not pretrained:
         model.eval()
         return model
@@ -56,3 +81,16 @@ def soundstream_16khz(checkpoint=None, pretrained=True, **kwargs):
     model.load_state_dict(state_dict['state_dict'], strict=False)
     model.eval()
     return model
+
+
+def test():
+    model = soundstream_16khz(checkpoint=r"C:\\Users\\dudni\\Downloads\\soundstream_16khz-20230425.ckpt"
+                              , pretrained=True)
+    x, sr = librosa.load(r"C:\\Study\\Thesis\\Test_compression\\test.flac", sr=16000)
+    y = model(x)
+
+    return y
+
+
+if __name__ == "__main__":
+    test()
--- soundstream_net.py
+++ soundstream_net.py
@@ -408,39 +408,6 @@ class ReconstructionLoss(nn.Module):
         return loss / (12 - 6)
 
 
-class ReconstructionLoss2(nn.Module):
-    """Reconstruction loss from https://arxiv.org/pdf/2107.03312.pdf
-    """
-    def __init__(self, sample_rate, eps=1e-5):
-        super().__init__()
-        import torchaudio
-        self.layers = nn.ModuleList()
-        self.alpha = []
-        self.eps = eps
-        for i in range(6, 12):
-            melspec = torchaudio.transforms.MelSpectrogram(
-                sample_rate=sample_rate,
-                n_fft=int(2 ** i),
-                win_length=int(2 ** i),
-                hop_length=int(2 ** i / 4),
-                n_mels=64)
-            self.layers.append(melspec)
-            self.alpha.append((2 ** i / 2) ** 0.5)
-
-    def forward(self, input, target):
-        loss = 0
-        for alpha, melspec in zip(self.alpha, self.layers):
-            x = melspec(input)
-            y = melspec(target)
-            if x.shape[-1] > y.shape[-1]:
-                x = x[:, y.shape[-1]]
-            elif x.shape[-1] < y.shape[-1]:
-                y = y[:, x.shape[-1]]
-            loss += torch.mean(torch.abs(x - y))
-            loss += alpha * torch.mean(torch.square(torch.log(x + self.eps) - torch.log(y + self.eps)))
-        return loss
-
-
 class StreamableModel(pl.LightningModule):
     def __init__(
         self,
